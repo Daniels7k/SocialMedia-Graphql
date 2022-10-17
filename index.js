eee68614e8a0dba68bfbd69470034994825e6e01
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-10-17 15:44:43 -0300

    entry point server application

---
+++ index.js
@@ -0,0 +1,18 @@
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose.connect(MONGODB).then(() => {
+  console.log("MongoDB Connected");
+  server.listen({ port: 5000 }).then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
+});
