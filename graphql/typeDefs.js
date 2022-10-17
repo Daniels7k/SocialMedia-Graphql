847f469b0442558eb2f3e7d7a3818409818005e6
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-10-17 17:47:09 -0300

    setting typedefs to register user

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -8,8 +8,27 @@ const typeDefs = gql`
     username: String!
   }
 
+  type User {
+    id: ID!
+    email: String!
+    token: String!
+    username: String!
+    createdAt: String!
+  }
+
+  input RegisterInput {
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+  }
+
   type Query {
     posts: [Post]
+  }
+
+  type Mutation {
+    register(registerInput: RegisterInput): User!
   }
 `;
 
